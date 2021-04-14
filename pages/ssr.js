9d9904825235ab55dd641eb06fd57130b472181c
WDever <wdever0279@gmail.com>
2021-04-14 17:47:46 +0900

    Add test page

--- pages/ssr.js
+++ pages/ssr.js
@@ -17,17 +17,5 @@ const SSRPage = () => (
   </App>
 )
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
-
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  })
-
-  return addApolloState(apolloClient, {
-    props: {},
-  })
-}
 
 export default SSRPage
